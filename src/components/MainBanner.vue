e1639a57fddfa67be21219724cb3fa52a47f90ab
Dmitriy Matiuk <matiuk.dm@gmail.com>
2023-08-31 19:14:49 +0300

    feat: change images path

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -35,19 +35,19 @@ import { ref } from 'vue'
 const data = [
   {
     name: 1,
-    src: '/src/assets/images/31409219-4c9f-4e67-b7e8-e46f15ddb718-english2.jpg'
+    src: 'https://dzhw0dewjr7dm.cloudfront.net/31409219-4c9f-4e67-b7e8-e46f15ddb718-english2.jpg'
   },
   {
     name: 2,
-    src: '/src/assets/images/bff45612-b3f2-408d-bfb9-399c9e5170dc-english4.jpg'
+    src: 'https://dzhw0dewjr7dm.cloudfront.net/cb20f866-088a-42f7-b617-f5a57300595d-english3.jpg'
   },
   {
     name: 3,
-    src: '/src/assets/images/cb20f866-088a-42f7-b617-f5a57300595d-english3.jpg'
+    src: 'https://dzhw0dewjr7dm.cloudfront.net/fc269423-a966-47b9-839b-a4aa4381f668-english1.jpg'
   },
   {
     name: 4,
-    src: '/src/assets/images/fc269423-a966-47b9-839b-a4aa4381f668-english1.jpg'
+    src: 'https://dzhw0dewjr7dm.cloudfront.net/bff45612-b3f2-408d-bfb9-399c9e5170dc-english4.jpg'
   }
 ]
 
